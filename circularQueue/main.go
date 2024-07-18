9557d17b442d54203f5b4cad69ff6f2f04dc9122
jopaleti <oopaletijohnson@gmail.com>
2024-07-18 19:20:24 +0100

    circularQueue

--- circularQueue/main.go
+++ circularQueue/main.go
@@ -22,64 +22,64 @@ func Constructor(k int) MyCircularQueue {
 }
 
 // Enqueue: - Adding element to the rare or back of the list
-func (this *MyCircularQueue) EnQueue(value int) bool {
-	if this.IsFull() {
+func (thisqueue *MyCircularQueue) EnQueue(value int) bool {
+	if thisqueue.IsFull() {
 		return false
 	}
-	if this.IsEmpty() {
-		this.head = 0
+	if thisqueue.IsEmpty() {
+		thisqueue.head = 0
 	}
 	// The tail pointer is updated to point to the next position in the circular buffer.
-	this.tail = (this.tail + 1) % this.capacity
-	this.data[this.tail] = value
-	this.size++
+	thisqueue.tail = (thisqueue.tail + 1) % thisqueue.capacity
+	thisqueue.data[thisqueue.tail] = value
+	thisqueue.size++
 	return true
 }
 
 // Dequeue: Removing element from the front of the queue
-func (this *MyCircularQueue) DeQueue(value int) bool {
-	if this.IsEmpty() {
+func (thisqueue *MyCircularQueue) DeQueue(value int) bool {
+	if thisqueue.IsEmpty() {
 		return false
 	}
 	// If the head pointer is equal to the tail pointer, 
 	// it means the queue has only one element.
-	if this.head == this.tail {
-		this.head = -1
-		this.tail = -1
+	if thisqueue.head == thisqueue.tail {
+		thisqueue.head = -1
+		thisqueue.tail = -1
 	} else {
 		// If the queue has more than one element, the head pointer is updated 
 		// to point to the next position in the circular buffer. 
-		this.head = (this.head + 1) % this.capacity
+		thisqueue.head = (thisqueue.head + 1) % thisqueue.capacity
 	}
-	this.size--
+	thisqueue.size--
 	return true
 }
 
 // Checking the front element
-func (this *MyCircularQueue) Front() int {
-	if this.IsEmpty() {
+func (thisqueue *MyCircularQueue) Front() int {
+	if thisqueue.IsEmpty() {
 		return -1
 	}
-	return this.data[this.head]
+	return thisqueue.data[thisqueue.head]
 }
 
 // Checking for the rear or last element
-func (this *MyCircularQueue) Rear() int {
-	if this.IsEmpty() {
+func (thisqueue *MyCircularQueue) Rear() int {
+	if thisqueue.IsEmpty() {
 		return -1
 	}
-	return this.data[this.tail]
+	return thisqueue.data[thisqueue.tail]
 }
 
 // Checking if our circular list is empty
 // NOTE: If the queue is empty, size must be zero(0)
-func (this *MyCircularQueue) IsEmpty() bool {
-	return this.size == 0
+func (thisqueue *MyCircularQueue) IsEmpty() bool {
+	return thisqueue.size == 0
 }
 
 // Checking if our queue is full
-func (this *MyCircularQueue) IsFull() bool {
-	return this.size == this.capacity
+func (thisqueue *MyCircularQueue) IsFull() bool {
+	return thisqueue.size == thisqueue.capacity
 }
 
 func main() {
