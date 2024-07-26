377bd8a096c902edc5ccaf66a28421df2ca99a2c
jopaleti <oopaletijohnson@gmail.com>
2024-07-26 16:25:28 +0100

    jessie&Cookie--medium

--- Heap/Hackerrank/jessie&Cookie/main.go
+++ Heap/Hackerrank/jessie&Cookie/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // Defining the heap struct
 type MinHeap struct {
 	elements []int
@@ -22,4 +20,74 @@ func right(i int) int {
 	return 2*i + 2
 }
 
+// SWAP function declaration
+func (h *MinHeap) swap(first, second int) {
+	h.elements[first], h.elements[second] = h.elements[second], h.elements[first]
+}
 
+// Insert function declaration
+func (h *MinHeap) Insert(x int) {
+	h.elements = append(h.elements, x)
+	h.heapifyUp(len(h.elements)-1)
+}
+
+// Delete Function
+func (h *MinHeap) Delete(x int) {
+	for i, v := range h.elements {
+		if v == x {
+			h.elements[i] = h.elements[len(h.elements) - 1]
+			h.elements = h.elements[:len(h.elements)-1]
+			h.heapifyDown(i)
+			break
+		}
+	}
+}
+
+// GetMin function
+func (h *MinHeap) GetMin() int {
+	if len(h.elements) == 0 {
+		return -1 // Assuming non-negative integers in the heap
+	}
+	return h.elements[0]
+}
+
+// HeapifyUp Algorithm
+func (h *MinHeap) heapifyUp(index int) {
+	for index > 0 {
+		if h.elements[index] >= h.elements[parent(index)] {
+			break
+		}
+		h.swap(index, parent(index))
+		// Update the indices
+		index = parent(index)
+	}
+}
+
+// HeapifyDown Algorithm
+func (h *MinHeap) heapifyDown(index int) {
+	lastIndex := len(h.elements) - 1
+
+	for {
+		leftChild := left(index)
+		rightChild := right(index)
+		smallest := index
+
+		if leftChild <= lastIndex && h.elements[leftChild] < h.elements[smallest] {
+			smallest = rightChild
+		}
+		if rightChild <= lastIndex && h.elements[rightChild] < h.elements[smallest] {
+			smallest = rightChild
+		}
+		if smallest == index {
+			break
+		}
+		h.swap(index, smallest)
+		// Updates the indices
+		index = smallest
+	}
+}
+
+
+func main() {
+	
+}
