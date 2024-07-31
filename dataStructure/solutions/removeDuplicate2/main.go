f906512d882af68a48c75a23872c8472bef4588f
jopaleti <oopaletijohnson@gmail.com>
2024-07-31 04:31:18 +0100

    removeDuplicate2

--- dataStructure/solutions/removeDuplicate2/main.go
+++ dataStructure/solutions/removeDuplicate2/main.go
@@ -3,18 +3,24 @@ package main
 import "fmt"
 
 func removeDuplicates(nums []int) int {
-    if len(nums) == 0{
-        return 0
-    }
-    write := 2
-    for read := 2; read<len(nums); read++ {
-        if nums[read] != nums[write-2] {
-            nums[write] = nums[read]
-            write++
-        }
-    }
-    // Return the length of the array with the valid elements
-    return write
+    if len(nums) <= 2 {
+		return len(nums)
+	}
+
+	// Pointer to write the next valid element
+	write := 2
+
+	for read := 2; read < len(nums); read++ {
+		// Check if the current element is different from the element two places before
+		if nums[read] != nums[write-2] {
+			// Write the current element to the write position
+			nums[write] = nums[read]
+			write++
+		}
+	}
+
+	// Return the length of the array with the valid elements
+	return write
 }
 
 // Main function to test the removeDuplicates function
